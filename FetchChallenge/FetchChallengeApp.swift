81ae32356435ddad21b64930f4ab081ff134642b
Brady Robshaw <heybrady88@gmail.com>
2022-12-02 14:07:07 -0500

    Initial Commit

---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Brady Robshaw on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
